b17e6a91de1a453ffede0ec1ea88ae8021444c75
Fredrik Borgstein <fredrik.borgstein@outlook.com>
2024-03-03 12:34:12 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { marked } from "https://esm.sh/marked";
+import { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import sanitizeHtml from "sanitize-html-react";
+marked.use({ breaks: true, gfm: true, pedantic: false });
 
 function App() {
+  const [markdown, setMarkdown] = useState(
+    "# heading\n ## subheading \n ### sub-subheading \n\n **bold** \n\n *italic* \n\n `code` \n\n ```\n code block\n ``` \n\n [link](https://www.freecodecamp.org) \n\n > blockquote \n\n - list item \n\n 1. numbered list item \n\n ![alt text](https://www.markdown-here.com/img/icon256.png) \n\n `npm install marked` \n\n ```javascript \n const marked = require('marked'); \n console.log(marked('I am using markdown.')); \n ``` \n\n"
+  );
+
+  function handleChange(event) {
+    setMarkdown(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <heading className="container text-center">
+        <h1>Markdown Editor & Previewer</h1>
+      </heading>
+      <Row>
+        <Col>
+          <div className="container" id="editor-section">
+            <h2 className="editor-title">Editor</h2>
+            <textarea
+              id="editor"
+              value={markdown}
+              onChange={handleChange}
+              type="text"
+              rows={30}
+              cols={50}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="container" id="preview-section">
+            <h2 className="previewer-title">Previewer</h2>
+            <div id="inner-preview-section">
+              <div
+                id="preview"
+                dangerouslySetInnerHTML={{
+                  __html: sanitizeHtml(marked(markdown), {
+                    allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+                      "img",
+                      "#",
+                      "blockquote",
+                      "code",
+                      "pre",
+                      "h1",
+                      "h2",
+                      "h3",
+                    ]),
+                  }),
+                }}
+              ></div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
